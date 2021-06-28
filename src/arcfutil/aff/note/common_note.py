3cb40e265ba0933050c59a825e261e1bc63b10d3
feightwywx <kururinmiracle@outlook.com>
2021-06-28 22:40:14 +0800

    Add: 用list或tuple创建NoteGroup Add: 为NoteGroup引入了类型检查

--- src/arcfutil/aff/note/common_note.py
+++ src/arcfutil/aff/note/common_note.py
@@ -38,11 +38,31 @@ class NoteGroup(list):
     def __init__(self, *notes):
         list.__init__(self)
         super(NoteGroup, self).__init__()
-        self.extend(notes)
+        for each in notes:
+            if isinstance(each, (list, tuple)):
+                self.extend(each)
+            else:
+                self.append(each)
         self.type = type(self).__name__
 
     def __str__(self):
         return ''.join(str(x) + '\n' for x in self if x is not None)
+
+    def append(self, __object) -> None:
+        if isinstance(__object, (Note, NoteGroup)):
+            super(NoteGroup, self).append(__object)
+        else:
+            raise TypeError('can only append Note or NoteGroup (or their instances, not "{}") to {}'.format(
+                type(__object).__name__,
+                type(self).__name__
+            ))
+
+    def extend(self, __iterable) -> None:
+        if isinstance(__iterable, NoteGroup):
+            super(NoteGroup, self).extend(__iterable)
+        else:
+            for each in __iterable:  # 如果容器不可信，则用含有类型检查的self.append()代替self.extend()
+                self.append(each)
 
     def moveto(self, dest: int):
         for each in self:
