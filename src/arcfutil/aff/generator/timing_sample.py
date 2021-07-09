e657e330ce9a416adf5116a8d6e0881e7d4b2f6c
feightwywx <canis@direcore.xyz>
2021-07-09 19:51:52 +0800

    Add: timing_glitch(), timing_easing_linear() 支持指定bar参数

--- src/arcfutil/aff/generator/timing_sample.py
+++ src/arcfutil/aff/generator/timing_sample.py
@@ -8,7 +8,9 @@ from ..note import Timing
 from ..note import NoteGroup
 
 
-def timing_glitch(origin_t: int, dest_t: int, count: int, bpm_range: float) -> NoteGroup:
+def timing_glitch(
+        origin_t: int, dest_t: int, count: int, bpm_range: float, exact_bar: float = 4.00, zero_bar: float = 4.00
+) -> NoteGroup:
     destgroup = NoteGroup()
     count -= 1
     stept = (dest_t - origin_t) / count
@@ -16,17 +18,19 @@ def timing_glitch(origin_t: int, dest_t: int, count: int, bpm_range: float) -> N
     exactbpm = -bpm_range
     while t < dest_t:
         exactbpm = -exactbpm
-        destgroup.append(Timing(t, exactbpm))
-        destgroup.append(Timing(t + 1, 0))
+        destgroup.append(Timing(t, exactbpm, exact_bar))
+        destgroup.append(Timing(t + 1, 0, zero_bar))
         t += stept
     return destgroup
 
 
-def timing_easing_linear(origin_t: int, dest_t: int, origin_bpm: float, dest_bpm: float, count: int) -> NoteGroup:
+def timing_easing_linear(
+        origin_t: int, dest_t: int, origin_bpm: float, dest_bpm: float, count: int,  bar: float = 4.00
+) -> NoteGroup:
     destgroup = NoteGroup()
     count -= 1
     stept = (dest_t - origin_t) / count
     stepbpm = (dest_bpm - origin_bpm) / count
     for i in range(count + 1):
-        destgroup.append(Timing(int(origin_t + i * stept), origin_bpm + i * stepbpm))
+        destgroup.append(Timing(int(origin_t + i * stept), origin_bpm + i * stepbpm, bar))
     return destgroup
