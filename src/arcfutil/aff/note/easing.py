fc39f70ba0ddba9afc15470aecfabfd85505587e
feightwywx <kururinmiracle@outlook.com>
2021-09-16 19:14:40 +0800

    Add: aff.easing.bezier() 现在支持自定义控制点

--- src/arcfutil/aff/note/easing.py
+++ src/arcfutil/aff/note/easing.py
@@ -21,9 +21,51 @@ def cosine(percent):
     return cos((percent * (pi / 2)) + pi) + 1
 
 
-def bezier(percent):
-    t = percent
-    return 3 * (1 - t) * pow(t, 2) + pow(t, 3)  # Bezier公式 0 0 1 1
+# 参照了Webkit的贝塞尔缓动实现
+# https://trac.webkit.org/browser/trunk/Source/WebCore/platform/graphics/UnitBezier.h
+def bezier(percent, p1x: float = 1/3, p1y: float = 0, p2x: float = 2/3, p2y: float = 1) -> float:
+    cx = 3.0 * p1x;
+    bx = 3.0 * (p2x - p1x) - cx;
+    ax = 1.0 - cx -bx;
+    cy = 3.0 * p1y;
+    by = 3.0 * (p2y - p1y) - cy;
+    ay = 1.0 - cy - by;
+
+    def solve_curve_x(x: float) -> float:
+        t2 = x
+        for i in range(8):
+            x2 = ((ax * t2 + bx) * t2 + cx) * t2 - x
+            if abs(x2) < 1e-6:
+                return t2
+            d2 = (3.0 * ax * t2 + 2.0 * bx) * t2 + cx
+            if abs(d2) < 1e-6:
+                break
+            t2 = t2 - x2 / d2
+        
+        t0 = 0.0
+        t1 = 1.0
+        t2 = x
+
+        if t2 < t0:
+            return t0
+        if t2 > t1:
+            return t1
+
+        while t0 < t1:
+            x2 = ((ax * t2 + bx) * t2 + cx) * t2
+            if abs(x2 - x) < 1e-6:
+                return t2
+            if x > x2:
+                t0 = t2
+            else:
+                t1 = t2
+            t2 = (t1 - t0) * 0.5 + t0
+
+        return t2
+    
+    t = solve_curve_x(percent)
+        
+    return ((ay * t + by) * t + cy) * t
 
 
 # @__checker
