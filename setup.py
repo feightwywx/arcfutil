bf4114931c229a15cafe2874cadb8008cec43f58
direwolf <kururinmiracle@outlook.com>
2022-01-21 00:12:29 +0800

    Update 0.7.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="arcfutil",
-    version="0.6.4",
+    version="0.7.0",
     author=".direwolf",
     author_email="kururinmiracle@outlook.com",
     description="A Python module designed for processing Arcaea related files(.aff chart, songlist, etc.)",
