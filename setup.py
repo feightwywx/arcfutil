9103a2c51a0bd25a96145ff3b9bdd482bf06952a
feightwywx <kururinmiracle@outlook.com>
2023-09-26 21:35:22 +0800

    Update 0.10.2 Fix: 修复了bounce系的缓动函数

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='utf8') as fh:
 
 setuptools.setup(
     name="arcfutil",
-    version="0.10.1",
+    version="0.10.2",
     author=".direwolf",
     author_email="kururinmiracle@outlook.com",
     description="A Python module designed for processing Arcaea related files(.aff chart, songlist, etc.)",
--- src/arcfutil/aff/easing/cheatsheet.py
+++ src/arcfutil/aff/easing/cheatsheet.py
@@ -71,14 +71,14 @@ def ease_out_bounce(x: float) -> float:
     n1 = 7.5625
     d1 = 2.75
 
-    if x < 1 / d1:
+    if x < (1 / d1):
         return n1 * x * x
-    elif x < 2 / d1:
-        return n1 * ((x - 1.5) / d1) * x + 0.75
-    elif x < 2.5 / d1:
-        return n1 * ((x - 2.25) / d1) * x + 0.9375
+    elif x < (2 / d1):
+        return n1 * (x - 1.5 / d1) ** 2 + 0.75
+    elif x < (2.5 / d1):
+        return n1 * (x - 2.25 / d1) ** 2 + 0.9375
     else:
-        return n1 * ((x - 2.625) / d1) * x + 0.984375
+        return n1 * (x - 2.625 / d1) ** 2 + 0.984375
 
 
 def ease_in_out_bounce(x: float) -> float:
